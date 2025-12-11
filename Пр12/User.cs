9db91f3c9433ea311a415eb236f41df82019dc53
arincha123 <a_egurnova@inbox.ru>
2025-12-11 12:57:11 +0300

    Создание класса User

---
+++ Пр12/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Пр12
+{
+    public static class User
+    {
+        static public string Model { get; set; } = "";
+        static public int ModelCost { get; set; }
+        static public string Type { get; set; } = "";
+        static public int TypeCost { get; set; }
+        static public string Color { get; set; } = "";
+        static public int ColorCost { get; set; }
+        static public List<string> Options { get; set; } = new List<string>();
+        static public string StrOptions { get; set; } = "";
+        static public int OptionsCost { get; set; }
+        static public int TOTAL { get; set; }
+        static public int BASE { get; set; }
+        static public int ADD { get; set; }
+        static public string Name { get; set; } = "";
+        static public string Phone { get; set; } = "";
+        static public string Email { get; set; } = "";
+
+    }
+}
