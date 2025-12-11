46f9701f4b30ef496c6004edd2818c12fc1e9dc1
arincha123 <a_egurnova@inbox.ru>
2025-12-11 13:00:26 +0300

    Создание Page1 + обработка добавления данных в User

--- Пр12/Pages/Page1.xaml.cs
+++ Пр12/Pages/Page1.xaml.cs
@@ -24,5 +24,68 @@ namespace Пр12.Pages
         {
             InitializeComponent();
         }
+
+        private void Forward_Click(object sender, RoutedEventArgs e)
+        {
+
+            NavigationService.Navigate(new Page2());
+            MainWindow.PlusProgress();
+
+            if (NavigationService?.CanGoForward == true)
+            {
+                NavigationService.GoForward();
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.MinusProgress();
+            if (NavigationService?.CanGoBack == true)
+            {
+                NavigationService?.GoBack();
+            }
+        }
+
+        private void Model_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            switch (radioButton.Content.ToString())
+            {
+                case "Седан":
+                    User.Model = "Седан";
+                    User.ModelCost = 1500000;
+                    break;
+                case "Хэтчбек":
+                    User.Model = "Хэтчбек";
+                    User.ModelCost = 1700000;
+                    break;
+                case "Внедорожник":
+                    User.Model = "Внедорожник";
+                    User.ModelCost = 2500000;
+                    break;
+
+            }
+        }
+
+        private void Type_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            switch (radioButton.Content.ToString())
+            {
+                case "Бензиновый 1.5 л (120 л. с.)":
+                    User.Type = "Бензиновый 1.5 л (120 л. с.)";
+                    User.TypeCost = 100000;
+                    break;
+                case "Бензиновый 2.0 л (150 л. с.)":
+                    User.Type = "Бензиновый 2.0 л (150 л. с.)";
+                    User.TypeCost = 150000;
+                    break;
+                case "Гибрид 2.5 л (200 л. с.)":
+                    User.Type = "Гибрид 2.5 л (200 л. с.)";
+                    User.TypeCost = 300000;
+                    break;
+
+            }
+        }
     }
 }
