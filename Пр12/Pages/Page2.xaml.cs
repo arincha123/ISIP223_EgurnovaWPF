139c18d448cac2273bbf58a08654da032d96efd6
arincha123 <a_egurnova@inbox.ru>
2025-12-11 13:04:40 +0300

    Добавление Page1 + дозапись User для обработки нажатия на CheckBox

--- Пр12/Pages/Page2.xaml.cs
+++ Пр12/Pages/Page2.xaml.cs
@@ -24,5 +24,105 @@ namespace Пр12.Pages
         {
             InitializeComponent();
         }
+
+        private void Forward_Click(object sender, RoutedEventArgs e)
+        {
+            User.UpdateStrOptions();
+            User.StrOptions = string.Join<string>("\n", User.Options);
+            NavigationService.Navigate(new Page3());
+            MainWindow.PlusProgress();
+
+            if (NavigationService?.CanGoForward == true)
+            {
+                NavigationService.GoForward();
+            }
+
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            User.UpdateStrOptions();
+            MainWindow.MinusProgress();
+            if (NavigationService?.CanGoBack == true)
+            {
+                NavigationService?.GoBack();
+            }
+        }
+
+        private void Color_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            switch (radioButton.Content.ToString())
+            {
+                case "Чёрный цвет (+200)":
+                    User.Color = "Чёрный цвет (+200)";
+                    User.ColorCost = 200;
+                    break;
+                case "Красный цвет (+500)":
+                    User.Color = "Красный цвет (+500)";
+                    User.ColorCost = 500;
+                    break;
+                case "Оранжевый цвет (+500)":
+                    User.Color = "Оранжевый цвет (+500)";
+                    User.ColorCost = 500;
+                    break;
+                case "Жёлтый цвет (+500)":
+                    User.Color = "Жёлтый цвет (+500)";
+                    User.ColorCost = 500;
+                    break;
+                case "Зелёный цвет (+500)":
+                    User.Color = "Зелёный цвет (+500)";
+                    User.ColorCost = 500;
+                    break;
+                case "Голубой цвет (+500)":
+                    User.Color = "Голубой цвет (+500)";
+                    User.ColorCost = 500;
+                    break;
+                case "Синий цвет (+500)":
+                    User.Color = "Синий цвет (+500)";
+                    User.ColorCost = 500;
+                    break;
+                case "Фиолетовый цвет (+500)":
+                    User.Color = "Фиолетовый цвет (+500)";
+                    User.ColorCost = 500;
+                    break;
+            }
+        }
+
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            SyncCheckBoxesWithUserData();
+        }
+
+        private void SyncCheckBoxesWithUserData()
+        {
+            // Восстанавливаем состояние галочек из User.Options
+            Krisha.IsChecked = User.Options.Contains("панорамная крыша");
+            Salon.IsChecked = User.Options.Contains("кожаный салон");
+            Climat.IsChecked = User.Options.Contains("климат-контроль");
+            Cruise.IsChecked = User.Options.Contains("круиз-контроль");
+        }
+
+        private void Krisha_Click(object sender, RoutedEventArgs e)
+        {
+            User.UpdateOption("панорамная крыша", 8000, Krisha.IsChecked == true);
+        }
+
+        private void Climat_Click(object sender, RoutedEventArgs e)
+        {
+            User.UpdateOption("климат-контроль", 25000, Climat.IsChecked == true);
+        }
+
+        private void Cruise_Click(object sender, RoutedEventArgs e)
+        {
+            User.UpdateOption("круиз-контроль", 45000, Cruise.IsChecked == true);
+        }
+
+        private void Salon_Click(object sender, RoutedEventArgs e)
+        {
+            User.UpdateOption("кожаный салон", 120000, Salon.IsChecked == true);
+        }
+
     }
 }
--- Пр12/User.cs
+++ Пр12/User.cs
@@ -24,5 +24,52 @@ namespace Пр12
         static public string Phone { get; set; } = "";
         static public string Email { get; set; } = "";
 
+
+        public static void UpdateStrOptions()
+        {
+            StrOptions = string.Join(", ", Options);
+        }
+
+        public static void UpdateOption(string optionName, int price, bool isSelected)
+        {
+            if (isSelected)
+            {
+                if (!Options.Contains(optionName))
+                {
+                    OptionsCost += price;
+                    Options.Add(optionName);
+                }
+            }
+            else
+            {
+                if (Options.Contains(optionName))
+                {
+                    OptionsCost -= price;
+                    Options.Remove(optionName);
+                }
+            }
+            UpdateStrOptions();
+        }
+        public static void RecalculateOptionsCost()
+        {
+            OptionsCost = 0;
+            foreach (var option in Options)
+            {
+                OptionsCost += GetOptionPrice(option);
+            }
+            UpdateStrOptions();
+        }
+
+        private static int GetOptionPrice(string option)
+        {
+            switch (option)
+            {
+                case "панорамная крыша": return 8000;
+                case "кожаный салон": return 120000;
+                case "климат-контроль": return 25000;
+                case "круиз-контроль": return 45000;
+                default: return 0;
+            }
+        }
     }
 }
