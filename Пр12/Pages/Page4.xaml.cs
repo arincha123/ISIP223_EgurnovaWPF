05bba0c3ab06cf1afaa3460f13736a5a6b8ac3e4
arincha123 <a_egurnova@inbox.ru>
2025-12-11 13:12:06 +0300

    Добавление Page4

--- Пр12/Pages/Page4.xaml.cs
+++ Пр12/Pages/Page4.xaml.cs
@@ -20,9 +20,89 @@ namespace Пр12.Pages
     /// </summary>
     public partial class Page4 : Page
     {
+        private int mc, vznos, sc;
+        private double fpp, i;
         public Page4()
         {
             InitializeComponent();
+        }
+        private void Forward_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.PlusProgress();
+            NavigationService.Navigate(new Page5());
+
+            if (NavigationService?.CanGoForward == true)
+            {
+                NavigationService.GoForward();
+            }
+
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            if (NavigationService?.CanGoBack == true)
+            {
+                MainWindow.MinusProgress();
+                NavigationService?.GoBack();
+            }
+        }
+
+        private void FPText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox fpt = (TextBox)sender;
+            string text = fpt.Text;
+            fpp = int.Parse(text);
+
+        }
+
+        private void FPText_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.Text[0]);
+        }
+
+        private void CreditText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox sum = (TextBox)sender;
+            sc = int.Parse(sum.Text);
+        }
+        private void MPS_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            i = int.Parse(tb.Text);
+
+        }
+
+        private void raschet_Click(object sender, RoutedEventArgs e)
+        {
+            double fvznos = (User.TOTAL * fpp) / 100;
+            VznosText.Text = fvznos.ToString();
+
+            double S = User.TOTAL - fvznos;
+            Credit.Text = S.ToString();
+
+            double A = (S * (i * Math.Pow(1 + i, mc)) / Math.Pow(1 + i, mc) - 1);
+            ItogZnach.Text = A.ToString();
+        }
+
+
+        private void VznosText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox vt = (TextBox)sender;
+            string text = vt.Text;
+            vznos = int.Parse(text);
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (TextCounts == null)
+                return;
+            int newValue = (int)e.NewValue;
+            int oldValue = (int)e.OldValue;
+            int difValue = Math.Abs(newValue - oldValue);
+
+            TextCounts.Text = (newValue).ToString();
+            mc = newValue;
+
         }
     }
 }
