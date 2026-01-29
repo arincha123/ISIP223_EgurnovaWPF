c5b756c74604e14245af4393b52bc8cfcbcfd5bf
arincha123 <a_egurnova@inbox.ru>
2026-01-29 14:22:40 +0300

    Почти доделана первая страница

--- Пр12/Pages/MainPage.xaml.cs
+++ Пр12/Pages/MainPage.xaml.cs
@@ -20,9 +20,46 @@ namespace Пр12.Pages
     /// </summary>
     public partial class MainPage : Page
     {
+        private List <FILM> allFs;
+        private List<FILM> sorted;
         public MainPage()
         {
             InitializeComponent();
+            List<FILM> films = Core.Context.FILM.ToList();
+            Films.ItemsSource = films;
+            PageLoad();
+
+        }
+
+        private void PageLoad()
+        {
+
+            allFs = Core.Context.FILM.ToList();
+            Films.ItemsSource = allFs;
+        }
+
+        private void Search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            List<FILM> fILMs = Core.Context.FILM.ToList();
+            var search = Search.Text.ToLower();
+            fILMs = fILMs.Where(f => f.NAME_FILM.ToLower().Contains(Search.Text.ToLower())).ToList();
+            Films.ItemsSource = fILMs;
+        }
+
+        private void Sort_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton sb = (RadioButton)sender;
+            switch (sb.Content.ToString())
+            {
+                case "Названию":
+                    sorted = allFs.OrderBy(f => f.NAME_FILM).ToList();
+                    Films.ItemsSource = sorted;
+                    break;
+                case "Рейтингу":
+                    sorted = allFs.OrderByDescending(f => f.RATING_FILM).ToList();
+                    Films.ItemsSource = sorted;
+                    break;
+            }
         }
     }
 }
