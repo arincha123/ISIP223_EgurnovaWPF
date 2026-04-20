3703f7958ffb5c484b592b9e59ab9857a0860323
arincha123 <a_egurnova@inbox.ru>
2026-04-20 17:29:03 +0300

    Начата разработка страницы клиента

---
+++ Пр12/Pages/ClientPages/MainClientPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Пр12.Pages.ClientPages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainClientPage.xaml
+    /// </summary>
+    public partial class MainClientPage : Page
+    {
+        List<UserService> ServicesForUser;
+        public MainClientPage()
+        {
+            InitializeComponent();
+            MainWindow.get_Btn_Back.Visibility = Visibility.Visible;
+
+            ServicesForUser = Core.Context.UserService.ToList();
+            ZapisiList.ItemsSource = ServicesForUser;
+        }
+    }
+}
--- Пр12/Pages/StartPage.xaml.cs
+++ Пр12/Pages/StartPage.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Пр12.Pages.AdminPages;
+using Пр12.Pages.ClientPages;
 using Пр12.Pages.ManagerPages;
 using Пр12.Pages.MasterPages;
 using Пр12.Pages.Windows;
@@ -49,7 +50,7 @@ namespace Пр12.Pages
             //Window lrwin = new LogRegWindow();
             //lrwin.Show();
 
-            NavigationService.Navigate(new MainManagerPage());
+            NavigationService.Navigate(new MainClientPage());
         }
 
         private void Tovari_Click(object sender, RoutedEventArgs e)
